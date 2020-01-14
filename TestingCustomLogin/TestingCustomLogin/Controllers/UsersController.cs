4a86e7c8380362bc153f5a21c11ad287beccc195
Kurilo Dmitriy <fobosdem@gmail.com>
2020-01-14 14:48:31 +0200

    111

---
+++ TestingCustomLogin/TestingCustomLogin/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestingCustomLogin.CustomAuthentication;
+
+namespace TestingCustomLogin.Controllers
+{
+	public class UsersController : Controller
+	{
+		[CustomAuthorize(Roles = "User")]
+		// GET: Users  
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
